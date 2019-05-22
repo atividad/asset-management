c85e0609c784de353feb2b6727f0ca0d9a886f6e
Adonis Tividad <avtividad@gmail.com>
2019-05-22 18:07:06 +0400

    Updated index.html directories from /js/ to js/

--- js/store.1.js
+++ js/store.1.js
@@ -37,29 +37,29 @@ const store = new Vuex.Store({
                     context.commit('SET_ASSETS', response.data.assets)
                 })
                 */
-               let assets = [{ "assetId": 1, "assetLabel": "Asset 1", "assetDescription": "Description 1", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "John Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
-               { "assetId": 2, "assetLabel": "Asset 2", "assetDescription": "Description 2", "assetType": "Mobile", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:56.497" },
-               { "assetId": 3, "assetLabel": "Asset 3", "assetDescription": "Description 3", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "Jane Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
-               { "assetId": 4, "assetLabel": "Asset 4", "assetDescription": "Description 4", "assetType": "Tablet", "assetStatus": "In-repair", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:51.49" },
-               { "assetId": 5, "assetLabel": "Asset 5", "assetDescription": "Description 5", "assetType": "Tablet", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:06:13.9" },];
-                context.commit('SET_ASSETS', assets);
+            let assets = [{ "assetId": 1, "assetLabel": "Asset 1", "assetDescription": "Description 1", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "John Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
+                            { "assetId": 2, "assetLabel": "Asset 2", "assetDescription": "Description 2", "assetType": "Mobile", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:56.497" },
+                            { "assetId": 3, "assetLabel": "Asset 3", "assetDescription": "Description 3", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "Jane Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
+                            { "assetId": 4, "assetLabel": "Asset 4", "assetDescription": "Description 4", "assetType": "Tablet", "assetStatus": "In-repair", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:51.49" },
+                            { "assetId": 5, "assetLabel": "Asset 5", "assetDescription": "Description 5", "assetType": "Tablet", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:06:13.9" },];
+            context.commit('SET_ASSETS', assets);
         },
         async assetDelete(context, object) {
-            url = store.state.api + '/assets/'+ object.assetId ;
+            url = store.state.api + '/assets/' + object.assetId;
             await axios.delete(url)
-              .then(response => {
-                console.log('response.data.assets', response.data.assets)
-                context.commit('SET_ASSETS', response.data.assets)
-              })
+                .then(response => {
+                    console.log('response.data.assets', response.data.assets)
+                    context.commit('SET_ASSETS', response.data.assets)
+                })
         },
         async assetSave(context, object) {
             url = store.state.api + '/assets/';
             await axios.post(url, object.asset)
-              .then(response => {
-                console.log('response.data.assets', response.data.assets)
-                context.commit('SET_ASSETS', response.data.assets);
-                app.showModal = false;
-              })
+                .then(response => {
+                    console.log('response.data.assets', response.data.assets)
+                    context.commit('SET_ASSETS', response.data.assets);
+                    app.showModal = false;
+                })
         },
 
 
@@ -100,11 +100,11 @@ const store = new Vuex.Store({
             });
         },
         
-        */ 
-        
+        */
+
 
     },
-   
+
 
 })
 
